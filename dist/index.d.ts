7953ed58fdfd2cb6f97979ce118189c33f8676ca
Unna Kim <work@unnakim.info>
2021-08-27 13:13:00 +0900

    [feat] add automation workflow

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,22 +1,12 @@
 import sendForm from "./src/sendForm";
 declare const _default: {
-  sendForm: typeof sendForm;
-  SMS: {
-    sendAligoSmsRequest: (
-      params: import("./src/sms").AligoSendSMSParams
-    ) => Promise<import("./src/sms").AligoApiSmsSendResult>;
-    sendAligoSmsMassRequest: (
-      params: import("./src/sms").AligoSendSMSParams
-    ) => Promise<import("./src/sms").AligoApiSmsSendResult>;
-    listAligoSmsSent: (
-      params: import("./src/sms").AligoSendSMSParams
-    ) => Promise<import("./src/sms").AligoSmsSendListResult>;
-    listAligoSmsSendDetail: (
-      params: import("./src/sms").AligoSendSMSParams
-    ) => Promise<import("./src/sms").AligoApiSmsSendResult>;
-    getAligoSmsRemain: (
-      params: import("./src/sendForm").AligoAPICommonType
-    ) => Promise<import("./src/sms").AligoApiSmsSendResult>;
-  };
+    sendForm: typeof sendForm;
+    SMS: {
+        sendAligoSmsRequest: (params: import("./src/sms").AligoSendSMSParams) => Promise<import("./src/sms").AligoApiSmsSendResult>;
+        sendAligoSmsMassRequest: (params: import("./src/sms").AligoSendSMSParams) => Promise<import("./src/sms").AligoApiSmsSendResult>;
+        listAligoSmsSent: (params: import("./src/sms").AligoSendSMSParams) => Promise<import("./src/sms").AligoSmsSendListResult>;
+        listAligoSmsSendDetail: (params: import("./src/sms").AligoSendSMSParams) => Promise<import("./src/sms").AligoApiSmsSendResult>;
+        getAligoSmsRemain: (params: import("./src/sendForm").AligoAPICommonType) => Promise<import("./src/sms").AligoApiSmsSendResult>;
+    };
 };
 export default _default;
